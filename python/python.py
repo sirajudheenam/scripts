5daea54f290bbc10ab0871ebd3fc5423bf7243cf
Sirajudheen Mohamed Ali <sirajudheenam@gmail.com>
2017-10-17 21:43:12 +0200

    added argument input

--- python/python.py
+++ python/python.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 import os
+import sys
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
 
 def makemydir("/tmp/python-created-01"):
   try:
@@ -8,6 +12,6 @@ def makemydir("/tmp/python-created-01"):
     pass
   os.chdir("/tmp/python-created-01")
 
-mypath = "/tmp/python-created-02"
+mypath = str(sys.argv)
 if not os.path.isdir(mypath):
    os.makedirs(mypath)
