fdf03c4acc7fad3253cf5f9207ddfded35721f31
Sirajudheen Mohamed Ali <sirajudheenam@gmail.com>
2017-10-17 21:34:15 +0200

    added some stuff to ruby

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -1,3 +1,8 @@
+#!/usr/bin/env ruby
+###!/usr/bin/ruby
 require 'fileutils'
-FileUtils::mkdir_p '/tmp/ruby-created-this01'
-system 'mkdir', '-p', '/tmp/ruby-created-this02'
+ARGV.each do|a|
+  puts "Argument: #{a}"
+  FileUtils::mkdir_p "/tmp/#{a}"
+end
+system 'mkdir', '-p', '/tmp/ruby-created-this01'
